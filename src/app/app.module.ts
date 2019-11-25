9a582b85dfc0d17d3e1abf5bf28e7af565ab2edd
Dimitris Boutakoglou <tzimakos98@hotmail.com>
2019-11-25 21:21:54 +0200

    Pepega 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
+import { TodoComponent } from './components/todo/todo.component';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
     NgbModule,
-    FormsModule
-  ],
+    FormsModule,
+    SweetAlert2Module.forRoot(),
+
+
+],
   providers: [],
   bootstrap: [AppComponent]
 })
